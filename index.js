b747536b2f7186abc10a4b0ac4f494151f267f1e
Siosaia Fonua <sfonua10@churchofjesuschrist.org>
2019-07-26 22:17:34 -0600

    clean up celsius to fahrenheit algorithmn

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 //Convert Celsius to fahrenheit
 let formulas = '';
-
 function convertToF() {
   const celsius = document.getElementById("celsius").value;
   let fahrenheit = celsius * (9/5) + 32;
   formulas += `<p>${celsius}°Celsius = ${fahrenheit}°Fahrenheit</p>`
   document.getElementById("fahrenheit").innerHTML = formulas;
-  document.getElementById("celsius").value = 0;
+  document.getElementById("celsius").value = '';
 }
 
