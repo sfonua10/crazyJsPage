699c924ffb0eea8c23a95ad72e3bd3f88625881c
Siosaia Fonua <sfonua10@churchofjesuschrist.org>
2019-07-26 22:15:21 -0600

    store results on page and change result format

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 //Convert Celsius to fahrenheit
-
+let formulas = '';
 
 function convertToF() {
-  const cel = document.getElementById("celsius").value;
-  let fahrenheit = cel * (9/5) + 32;
-  document.getElementById("fahrenheit").innerHTML = fahrenheit += " fahrenheit";
+  const celsius = document.getElementById("celsius").value;
+  let fahrenheit = celsius * (9/5) + 32;
+  formulas += `<p>${celsius}°Celsius = ${fahrenheit}°Fahrenheit</p>`
+  document.getElementById("fahrenheit").innerHTML = formulas;
+  document.getElementById("celsius").value = 0;
 }
 
