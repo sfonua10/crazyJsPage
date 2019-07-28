20134a3e5866f923015cebacb9be2a9a1c687aeb
Siosaia Fonua <sfonua10@churchofjesuschrist.org>
2019-07-27 19:18:42 -0600

    factorialize number algorithmn

--- index.js
+++ index.js
@@ -22,3 +22,51 @@ document.addEventListener('keydown', e => {
   }
 })
 //Reverse A String END
+
+//Factorialize A Number
+//There is a problem with the input tag, it's currently lagging in the browser
+//to display the user input
+function factorializeNumber() {
+  let userInput = document.getElementById('factorializeInput').value
+  document.getElementById('numToFac').innerHTML = userInput;
+}
+
+function factorialize(num) {
+  if(num === 0) {
+    return 1
+  }
+  let newArr = [];
+  for(let i = 1; i <= num; i++) {
+    newArr.push(i);
+  }
+  const newNum = newArr.reduce((a,b) => a * b)
+  return newNum;
+}
+
+function randomizeNumber() {
+  const randomNum = (Math.floor(Math.random() * 20))
+  document.getElementById('numToFac').innerHTML = randomNum;
+  document.getElementById('facNum').innerHTML = ""; 
+  const facNum = factorialize(randomNum);
+  let newStr = " ";
+  setTimeout(() => { 
+    newStr += " - "
+    document.getElementById('facNum').innerHTML = newStr; 
+  }, 1000);
+  setTimeout(() => { 
+    newStr += " - "
+    document.getElementById('facNum').innerHTML = newStr; 
+  }, 2000);
+  setTimeout(() => { 
+    newStr += " - "
+    document.getElementById('facNum').innerHTML = newStr; 
+  }, 3000);
+  setTimeout(() => { 
+    newStr += " > "
+    document.getElementById('facNum').innerHTML = newStr; 
+  }, 4000);
+  setTimeout(() => { 
+    newStr += facNum
+    document.getElementById('facNum').innerHTML = newStr; 
+  }, 5000);
+}
